755bbe8e8a748dd2dab771a03be01bc39bd5a7a4
Gurulakshmi-123 <gurulakshmi2897@gmail.com>
2022-08-17 15:56:46 +0530

    comparison operators concept

---
+++ com.ope.js
@@ -0,0 +1,32 @@
+var r1 = 20 > 10; 
+var r2 = 20 < 10; 
+var r3 = 10 == 10;
+console.log(r1);
+console.log(r2);
+console.log(r3);
+
+var a = 10, 
+    b = 20; 
+
+console.log(a >= b);  
+console.log(a == 10); 
+
+
+
+let name1 = 'alice',
+    name2 = 'bob';    
+
+let result = name1 > name2
+console.log(result); 
+console.log(name1 == 'alice'); 
+
+
+
+console.log(true > 0); // true
+console.log(false < 1); // true
+console.log(true > false); // true   //boolean values
+console.log(false > true); // false
+console.log(true >= true); // true
+console.log(true <= true); // true
+console.log(false <= false); // true
+console.log(false >= false); // true
